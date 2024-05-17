1c9fd257816e49d43daf81e56ed7ea753c74422c
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-05-17 20:33:08 -0300

    a

--- src/conjuntoPiezas.cpp
+++ src/conjuntoPiezas.cpp
@@ -1,10 +1,35 @@
 #include "../include/conjuntoPiezas.h"
 
-TConjuntoPiezas crearTConjuntoPiezas(int cantMax){ return NULL; }
+struct rep_conjuntopiezas{
+    int* ids;
+    int cantidad;
+    int max;
+};
 
-bool esVacioTConjuntoPiezas(TConjuntoPiezas c){ return false; }
+TConjuntoPiezas crearTConjuntoPiezas(int cantMax){
+    TConjuntoPiezas nuevo = new rep_conjuntopiezas;
+    nuevo->ids = new int[cantMax];
+    for (int i = 0; i < cantMax; i++){
+        nuevo->ids[i] = 0;
+    }
+    nuevo->cantidad = 0;
+    nuevo->max = cantMax;
+    return nuevo;
+}
 
-void insertarTConjuntoPiezas(TConjuntoPiezas &c, int id){}
+bool esVacioTConjuntoPiezas(TConjuntoPiezas c){
+    
+    return false;
+}
+
+void insertarTConjuntoPiezas(TConjuntoPiezas &c, int id){
+    if (id < c->max){
+        if (c->ids[id] != 1){
+            c->ids[id] = 1;
+            c->cantidad++;
+        }
+    }
+}
 
 void borrarDeTConjuntoPiezas(TConjuntoPiezas &c, int id){}
 
@@ -14,9 +39,20 @@ int cardinalTConjuntoPiezas(TConjuntoPiezas c){ return 0; }
 
 int cantMaxTConjuntoPiezas(TConjuntoPiezas c){ return 0; }
 
-void imprimirTConjuntoPiezas(TConjuntoPiezas c){}
+void imprimirTConjuntoPiezas(TConjuntoPiezas c){
+    for (int i = 0; i < c->max; i++){
+        if(c->ids[i] == 1){
+            printf("%i ",i);
+        }
+    }
+    printf("\n");   
+}
 
-void liberarTConjuntoPiezas(TConjuntoPiezas &c){}
+void liberarTConjuntoPiezas(TConjuntoPiezas &c){
+    delete[] c->ids;
+    delete c;
+    c = NULL;
+}
 
 TConjuntoPiezas unionTConjuntoPiezas(TConjuntoPiezas c1, TConjuntoPiezas c2){ return NULL; }
 
