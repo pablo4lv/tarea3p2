97588536a470328bfb3d727e137e6c47f3d2ba4c
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-05-18 00:07:41 -0300

    a

--- src/listaExposiciones.cpp
+++ src/listaExposiciones.cpp
@@ -1,8 +1,32 @@
 #include "../include/listaExposiciones.h"
 
-TListaExposiciones crearTListaExposicionesVacia(){ return NULL; }
+struct rep_listaexposiciones{
+    TExposicion exposicion;
+    TListaExposiciones sig;
+};
 
-void agregarExposicionTListaExposiciones(TListaExposiciones &listaExposiciones, TExposicion expo){}
+TListaExposiciones crearTListaExposicionesVacia(){
+    return NULL;
+}
+
+void agregarExposicionTListaExposiciones(TListaExposiciones &listaExposiciones, TExposicion expo){
+    TListaExposiciones nuevo = new rep_listaexposiciones;
+    nuevo->exposicion = expo;
+    nuevo->sig = NULL;
+
+    //Si se inserta al principio, sea vacía o no la lista
+    if (listaExposiciones == NULL || fechaInicioTExposicion(expo) < fechaInicioTExposicion(listaExposiciones->exposicion)){
+        nuevo->sig = listaExposiciones;
+        listaExposiciones = nuevo;
+    } else {
+        TListaExposiciones aux = listaExposiciones;
+        while (aux->sig != NULL && fechaInicioTExposicion(expo) >= fechaInicioTExposicion(aux->sig->exposicion)){
+            aux = aux->sig;
+        }
+        nuevo->sig = aux->sig;
+        aux->sig = nuevo;
+    }
+}
 
 bool perteneceExposicionTListaExposiciones(TListaExposiciones listaExposiciones, int idExpo){ return false; }
 
@@ -10,9 +34,25 @@ TExposicion obtenerExposicionTListaExposiciones(TListaExposiciones listaExposici
 
 bool esVaciaTListaExposiciones(TListaExposiciones listaExposiciones){ return false; }
 
-void imprimirTListaExposiciones(TListaExposiciones listaExposiciones){}
+void imprimirTListaExposiciones(TListaExposiciones listaExposiciones){
+    while (listaExposiciones != NULL){
+        imprimirTExposicion(listaExposiciones->exposicion);
+        listaExposiciones = listaExposiciones->sig;
+    }
+}
 
-void liberarTListaExposiciones(TListaExposiciones &listaExposiciones, bool liberarExposiciones){}
+void liberarTListaExposiciones(TListaExposiciones &listaExposiciones, bool liberarExposiciones){
+    if (liberarExposiciones){
+        TListaExposiciones aux = listaExposiciones;
+        while (listaExposiciones != NULL){
+            listaExposiciones = listaExposiciones->sig;
+            delete aux;
+            aux = aux->sig;
+        }
+    }
+    delete listaExposiciones;
+    listaExposiciones = NULL;
+}
 
 TListaExposiciones obtenerExposicionesFinalizadas(TListaExposiciones &listaExposiciones, TFecha fecha){ return NULL; }              
 
