bb2088ed993bca1423a223b17ce6b56e5578e266
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-05-17 23:37:45 -0300

    a

--- src/exposicion.cpp
+++ src/exposicion.cpp
@@ -48,7 +48,7 @@ TFecha fechaFinTExposicion(TExposicion exp){
 
 bool sonExposicionesCompatibles(TExposicion exp1, TExposicion exp2){
     //Si se superponen
-    if ((compararTFechas(exp1->fin,exp2->inicio) && (compararTFechas(exp1->inicio,exp2->fin))!=1) || compararTFechas(exp2->fin,exp1->inicio)){
+    if (!(compararTFechas(exp1->fin,exp2->inicio) < 0 || compararTFechas(exp2->fin,exp1->inicio) < 0)){
         //y tienen piezas en comun
         TConjuntoPiezas interseccion = interseccionTConjuntoPiezas(exp1->conjunto,exp2->conjunto);
         if(!esVacioTConjuntoPiezas(interseccion)){
