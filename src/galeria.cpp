1ac2b854253411e5ae3774b624fe26837b06e5c5
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-05-30 10:27:43 -0300

    a

--- src/galeria.cpp
+++ src/galeria.cpp
@@ -23,20 +23,50 @@ void agregarPiezaTGaleria(TGaleria galeria, TPieza pieza){
 }
 
 void agregarExposicionTGaleria(TGaleria galeria, TExposicion expo){
-
+    if (compararTFechas(galeria->fecha,fechaInicioTExposicion(expo)) < 0){
+        agregarExposicionTListaExposiciones(galeria->futuras, expo);
+    } else {
+        if (compararTFechas(galeria->fecha, fechaInicioTExposicion(expo)) >= 0){
+            agregarExposicionTListaExposiciones(galeria->activas,expo);
+        } else {
+            agregarExposicionTListaExposiciones(galeria->finalizadas, expo);
+        }
+    }
 }
 
-void agregarPiezaAExposicionTGaleria(TGaleria galeria, int idPieza, int idExpo){}
+void agregarPiezaAExposicionTGaleria(TGaleria galeria, int idPieza, int idExpo){
+    TPieza pieza = obtenerPiezaColeccionPiezas(galeria->coleccion,idPieza);
+    TExposicion destino = NULL;
+    if (perteneceExposicionTListaExposiciones(galeria->activas, idExpo)){
+        destino = obtenerExposicionTListaExposiciones(galeria->activas,idExpo);
+    } else if (perteneceExposicionTListaExposiciones(galeria->futuras, idExpo)){
+        destino = obtenerExposicionTListaExposiciones(galeria->futuras,idExpo);
+    } else {
+        destino = obtenerExposicionTListaExposiciones(galeria->finalizadas,idExpo);
+    }
+    agregarATExposicion(destino, pieza);
+}
 
-void avanzarAFechaTGaleria(TGaleria galeria, TFecha fecha){}
+void avanzarAFechaTGaleria(TGaleria galeria, TFecha fecha){
+    
+}
 
-void imprimirExposicionesFinalizadasTGaleria(TGaleria galeria){}
+void imprimirExposicionesFinalizadasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->finalizadas);
+}
 
-void imprimirExposicionesActivasTGaleria(TGaleria galeria){}
+void imprimirExposicionesActivasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->activas);
+}
 
-void imprimirExposicionesFuturasTGaleria(TGaleria galeria){}
+void imprimirExposicionesFuturasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->futuras);
+}
 
-bool esCompatibleExposicionTGaleria(TGaleria galeria, TExposicion expo){ return false; }
+bool esCompatibleExposicionTGaleria(TGaleria galeria, TExposicion expo){
+    
+    return false;
+}
 
 void liberarTGaleria(TGaleria &galeria){
     liberarColeccionPiezas(galeria->coleccion);
